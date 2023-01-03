3f4e755e9aebd3cd2dc35dc7aa52ffa8af0d4b5d
HFKitti <HFK9457@gmail.com>
2023-01-03 17:55:11 +0100

    xD

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -10,13 +10,9 @@ public class Game {
         String[] secondWord = {"egyet!", "kettőt!", "hármat!", "négyet!", "ötöt!", "hatot!"};
         String[] thirdWord = {"Ilyen ez a játék!", "Sok sikert!"};
 
-        int oneLenght = fisrtWord.length;
-        int secondLenght = secondWord.length;
-        int thirdLenght = thirdWord.length;
-
-        int random1 = (int) (Math.random()* oneLenght);
-        int random2 = (int) (Math.random()* secondLenght);
-        int random3 = (int) (Math.random()* thirdLenght);
+        int random1 = (int) (Math.random()* fisrtWord.length);
+        int random2 = (int) (Math.random()* secondWord.length);
+        int random3 = (int) (Math.random()* thirdWord.length);
 
         String sentence = fisrtWord[random1] + " " + secondWord[random2] + " " + thirdWord[random3];
 
